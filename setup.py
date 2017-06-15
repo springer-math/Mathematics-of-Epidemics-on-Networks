d9b6817b84dbf40c224f0594a9e2009bbda32652
joelmiller <joel.c.miller@gmail.com>
2017-06-15 13:54:29 -0700

    updating from 0.96rc3 to 0.96

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 from distutils.core import setup
 
 setup(name='EoN',
-      version='0.96rc3',
+      packages = ['EoN'], 
+      version='0.96',
       description = 'Epidemics on Networks',
       author = 'Joel C. Miller, Istvan Z. Kiss, and Peter Simon',
       author_email = 'joel.c.miller.research@gmail.com',
       url = 'https://springer-math.github.io/Mathematics-of-Epidemics-on-Networks/',
+      download_url = 'https://github.com/springer-math/Mathematics-of-Epidemics-on-Networks/archive/0.96.tar.gz',
+      keywords = ['Epidemics on Networks', 'Epidemic Sonnet Works'],
       )
