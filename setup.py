9526266d9223d7a7fde1367147466a24f7b42673
joelmiller <joel.c.miller@gmail.com>
2017-06-16 10:44:12 -0700

    update version names and add tag for pypi release

--- setup.py
+++ setup.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 
+r'''
+Setup script for EoN (Epidemics on Networks)
+
+to install from this script, run
+
+    python setup.py install
+
+Alternately, you can install with pip.
+    
+    pip install EoN
+    
+If this is a "release candidate" (has an "rc" in the version name below), then
+pip will download the previous version - see the download_url below.
+'''
+
 from distutils.core import setup
 
 setup(name='EoN',
       packages = ['EoN'], 
-      version='0.97rc1',
+      version='0.97.1',  #http://semver.org/
       description = 'Epidemics on Networks',
       author = 'Joel C. Miller, Istvan Z. Kiss, and Peter Simon',
       author_email = 'joel.c.miller.research@gmail.com',
       url = 'https://springer-math.github.io/Mathematics-of-Epidemics-on-Networks/',
-      download_url = 'https://github.com/springer-math/Mathematics-of-Epidemics-on-Networks/archive/0.96.tar.gz',
+      download_url = 'https://github.com/springer-math/Mathematics-of-Epidemics-on-Networks/archive/0.97.1.tar.gz',
       keywords = ['Epidemics on Networks', 'Epidemic Sonnet Works'],
       install_requires = [
           'networkx',
