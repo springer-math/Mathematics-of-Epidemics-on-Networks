a311c4fb510079a7412ffdc03e54c5cb7ce3389b
joelmiller <joel.c.miller@gmail.com>
2018-01-03 23:01:43 +1100

    updating to 0.99.0 (removing rc1).  Now installable through pip

--- EoN/__init__.py
+++ EoN/__init__.py
@@ -38,7 +38,7 @@ We start with a few useful auxiliary functions
 '''
 
 __author__ = "Joel C. Miller, Istvan Z. Kiss, and Peter Simon"
-__version__ = "0.99.0rc1"
+__version__ = "0.99.0"
 
 
 #print("warning - EoN is currently under significant development.  Interface"
