d01bdb0e6606f608829e7d13d7c9ba3400dff5fe
tinghf <tinghf@gmail.com>
2018-03-16 10:45:41 -0700

    remove PyCallGraph stuff from sweep_params tests as they are in test_sim_generate_callgraphcs.py already;

--- EoN/tests/test_sim_sweep_parameters.py
+++ EoN/tests/test_sim_sweep_parameters.py
@@ -1,6 +1,4 @@
 from EoN import *
-from pycallgraph import PyCallGraph
-from pycallgraph.output import GraphvizOutput
 
 import numpy as np
 import networkx as nx
@@ -10,7 +8,6 @@ G = nx.grid_2d_graph(50, 50)  # each node is (u,v) where 0<=u,v<=99
 initial_infections = [(u, v) for (u, v) in G if 23 < u < 27 and 23 < v < 27]
 
 class TestSimSweepParameters:
-    graphviz = GraphvizOutput()
 
     @classmethod
     def setup_class(cls):
@@ -29,33 +26,23 @@ class TestSimSweepParameters:
         assert n % 2 == 0 or nn % 2 == 0
 
     def test_Gillespie_SIS_type(self):
-        TestSimSweepParameters.graphviz.output_file = 'Gillespie_SIS1.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            t, S, I = EoN.Gillespie_SIS(G, 1.0, 1.0, initial_infecteds=initial_infections, return_full_data=False, tmax=10)
+        t, S, I = EoN.Gillespie_SIS(G, 1.0, 1.0, initial_infecteds=initial_infections, return_full_data=False, tmax=10)
 
         assert t.__len__() == 3545
         assert S.__len__() == 3545
         assert I.__len__() == 3545
 
     def test_Gillespie_SIS_sweep_gamma(self):
-        TestSimSweepParameters.graphviz.output_file = 'Gillespie_SIS.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            [EoN.Gillespie_SIS(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
+        [EoN.Gillespie_SIS(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
 
     def test_Gillespie_SIR_sweep_gamma(self):
-        TestSimSweepParameters.graphviz.output_file = 'Gillespie_SIR.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            [EoN.Gillespie_SIR(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
+        [EoN.Gillespie_SIR(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
 
     def test_fast_SIS_sweep_gamma(self):
-        TestSimSweepParameters.graphviz.output_file = 'Fast_SIS.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            [EoN.fast_SIS(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
+        [EoN.fast_SIS(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
 
     def test_fast_SIR_sweep_gamma(self):
-        TestSimSweepParameters.graphviz.output_file = 'Fast_SIR.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            [EoN.fast_SIR(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
+        [EoN.fast_SIR(G, 1.0, i, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
 
     def test_Gillespie_SIS_sweep_tau(self):
         [EoN.Gillespie_SIS(G, i, 1.0, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
@@ -70,12 +57,8 @@ class TestSimSweepParameters:
         [EoN.fast_SIR(G, i, 1.0, initial_infecteds=initial_infections, return_full_data=True, tmax=10) for i in np.arange(0.0, 1.0, 0.1)]
 
     def test_basic_discrete_SIS_sweep_p(self):
-        TestSimSweepParameters.graphviz.output_file = 'Basic_discrete_SIS.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            [EoN.basic_discrete_SIS(G, i, initial_infecteds=initial_infections, rho = None, tmin = 0, tmax = 10, return_full_data = True) for i in np.arange(0.0, 1.0, 0.1)]
+        [EoN.basic_discrete_SIS(G, i, initial_infecteds=initial_infections, rho = None, tmin = 0, tmax = 10, return_full_data = True) for i in np.arange(0.0, 1.0, 0.1)]
 
     def test_basic_discrete_SIR_sweep_p(self):
-        TestSimSweepParameters.graphviz.output_file = 'Basic_discrete_SIR.png'
-        with PyCallGraph(output=TestSimSweepParameters.graphviz):
-            [EoN.basic_discrete_SIR(G, i, initial_infecteds=initial_infections, rho = None, tmin = 0, tmax = 10, return_full_data = True) for i in np.arange(0.0, 1.0, 0.1)]
+        [EoN.basic_discrete_SIR(G, i, initial_infecteds=initial_infections, rho = None, tmin = 0, tmax = 10, return_full_data = True) for i in np.arange(0.0, 1.0, 0.1)]
 
