388592af3dacef2a0cc5cf86e8d26eef7e0d7103
joelmiller <joel.c.miller@gmail.com>
2019-12-05 11:18:05 +1100

    reverting setup.py to state 1.0.9rc2 (had changed to 1.0.9) now that it's clear that the issue with readthedocs was from networkx's setup.py, not ours.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(name='EoN',
       packages = ['EoN'], 
-      version='1.0.9',  #http://semver.org/
+      version='1.0.9rc2',  #http://semver.org/
       description = 'Epidemics on Networks',
       author = 'Joel C. Miller, Istvan Z. Kiss, and Peter Simon',
       author_email = 'joel.c.miller.research@gmail.com',
